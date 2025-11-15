3659190cebda3acf4db1b31b1ff28e4730635020
kmanalang2 <kmanalang2@toromail.csudh.edu>
2025-11-14 23:52:45 -0800

    Update Stats.h
    added necessary functions, however did not include median or mode

--- Stats.h
+++ Stats.h
@@ -1,3 +1,18 @@
+#ifndef STATS_H
+#define STATS_H
 
-// display the statistics of the class all at once (Reason: avoid unnecessary function calls)
+# include "Gradebook.h" // We include this so that we have access to the struct "Student".
+
+// 1. display the statistics of the class all at once (Reason: avoid unnecessary function calls)
 void ComputeStats();
+
+// 2. Mean function --> Calculates the average of all grades
+double mean(Student s);
+
+// 3. Minimum --> Finds the lowest grade of all students
+double min(Student s);
+
+// 4. Maximum --> Finds the highest grade of all students
+double max(Student s);
+
+#endif
