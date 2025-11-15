8f8a13eae09e116ce86ad96e00352b7d8ff401e7
kmanalang2 <kmanalang2@toromail.csudh.edu>
2025-11-15 00:05:21 -0800

    Updated Stats.h, Updated Stats.c

--- Stats.c
+++ Stats.c
@@ -1,1 +1,30 @@
+#include "Stats.h" // note: we already did "#include Gradebook.h" inside Stats.h, so no need to do it again.
 
+// 1. Display all the statistical analysis
+void computeStats(Student s)
+{
+  double meanGrade = mean(s);
+  double minGrade = min(s);
+  double maxGrade = max(s);
+}
+
+// 2. Get the mean grade
+double mean(Student s)
+{
+
+
+}
+
+// 3. Get the lowest grade
+double min(Student s)
+{
+
+
+}
+
+// 4. Get the highest grade
+double max(Student s)
+{
+
+
+}
--- Stats.h
+++ Stats.h
@@ -4,7 +4,7 @@
 # include "Gradebook.h" // We include this so that we have access to the struct "Student".
 
 // 1. display the statistics of the class all at once (Reason: avoid unnecessary function calls)
-void ComputeStats();
+void ComputeStats(Student s); // must pass struct Student, so that we can pass it to the functions inside this one
 
 // 2. Mean function --> Calculates the average of all grades
 double mean(Student s);
