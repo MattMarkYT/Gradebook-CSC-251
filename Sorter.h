bdadaebd90eae2912eca67c18b555dfb131c9fee
ggonzalez332 <157663328+ggonzalez332@users.noreply.github.com>
2025-11-17 11:26:01 -0800

    Update Sorter.h
    Deleted unnecessary additions for readability.

--- Sorter.h
+++ Sorter.h
@@ -1,9 +1,6 @@
-#ifndef SORTER_H
-#define SORTER_H
 
 
 /* uses insertion sort or selection sort to sort the students in a method chosen beforehand
  takes in an array and a key of int */
 void sort(int arr[], int n);
 
-#endif
