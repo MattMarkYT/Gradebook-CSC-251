c01e6ea2cdced336ff0601723f58ba24909f0550
kmanalang2 <kmanalang2@toromail.csudh.edu>
2025-11-16 22:15:30 -0800

    Updates to Stats.c and Stats.h
    Stats.h computeStats() is now a no arg. In Stats.c, I adjusted to no arg for computeStats(), fixed the getSize() issue, and made sure to use Students s[] since we are using a struct.

--- Stats.c
+++ Stats.c
@@ -1,11 +1,12 @@
 #include "Stats.h" // note: we already did "#include Gradebook.h" inside Stats.h, so no need to do it again.
 
 // 1. Display all the statistical analysis
-void computeStats(Student s)
+void computeStats() // no arg for simplicity
 {
-  double meanGrade = mean(s);
-  double minGrade = min(s);
-  double maxGrade = max(s);
+  // note: getStudents() comes from Gradebook file. This contains the array itself
+  double meanGrade = mean(getStudents());
+  double minGrade = min(getStudents());
+  double maxGrade = max(getStudents()));
 
   printf("Average grade for this course: %.2f\n", meanGrade);
   printf("Lowest grade for this course: %.2f\n", minGrade);
@@ -13,10 +14,10 @@ void computeStats(Student s)
 }
 
 // 2. Get the mean grade
-static double mean(Student s)
+static double mean(Student s[])
 {
   double avgGrade, sum;
-  int sizeOfClass = s.getSize();
+  int sizeOfClass = getSize();
   
   // loop through the entire struct (aka the array of students), which will allow us to obtain the sum of all grades
   for (int i = 0; i < sizeOfClass; i++)
@@ -31,10 +32,10 @@ static double mean(Student s)
 }
 
 // 3. Get the lowest grade
-static double min(Student s)
+static double min(Student s[])
 {
   double lowestGrade = s[0].grade; // for initialization, the lowest grade is at index 0. However, this will be changed by comparing to all the other students.
-  int sizeOfClass = s.getSize();
+  int sizeOfClass = getSize();
   // loop through the entire class, and compare the previous student to the next to see which has a lower grade. The lower grade of the two for that iteration
   // is the new min.
   for (int i = 1; i < sizeOfClass; i++)
@@ -49,10 +50,10 @@ static double min(Student s)
 }
 
 // 4. Get the highest grade
-static double max(Student s)
+static double max(Student s[])
 {
   double highestGrade = s[0].grade // for initialization, assume the highest grade is at index 0. This will change as we compare to all students in the database
-  int sizeOfClass = s.getSize();
+  int sizeOfClass = getSize();
   
   // loop through the entire class, and compare the previous student to the next. The higher grade of the two is the new highest grade.
   for (int i = 1; i < sizeOfClass; i++)
--- Stats.h
+++ Stats.h
@@ -4,6 +4,6 @@
 #include "gradebook.h"
 
 // 1. display the statistics of the class all at once (Reason: avoid unnecessary function calls)
-void ComputeStats(Student s); // must pass struct Student, so that we can pass it to the functions inside this one
-			      // in this case, we are passing by value
+void ComputeStats(); // must pass struct Student, so that we can pass it to the functions inside this one
+		     // in this case, we are passing by value. This is a no arg because we do not want dependency on inputting the struct array.
 #endif
