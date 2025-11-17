042e42ad936e593233b53a48dbfec100db9cde04
Godlesschinchilla <155940177+Godlesschinchilla@users.noreply.github.com>
2025-11-16 18:47:22 -0800

    Update Gradebook.c

--- Gradebook.c
+++ Gradebook.c
@@ -1,8 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "Gradebook.h"
 
-  void printStudent(Student s)
-{
+//Prints Student info
+   void printStudent(Student s)
+{  
   printf("Name: %s\n", s.name);
   printf("ID: %d\n", s.ID);
   printf("Grade: %.2f\n", s.grade);
+}
+
+
+//exapmle to show usage 
+// will be updated to get multiple student(s) later on
+int main(){
+  Student s;
+  printf("Enter the student(s) name: ");
+//  scanf("%c", &s.name);
+  printf("Enter student(s) ID: ");
+  //scanf("%d", &s.ID;
+  printf("Enter student(s) grade: ");
+//  scanf("%lf", &s.grade);
+
+   // Validate grade range
+    if (s.grade < 0.0 || s.grade > 100.0)
+    {
+        fprintf(stderr, "Error: Grade must be between 0 and 100.\n");
+        return 1;
+    }
+    printStudent(s);
+  return 0;
 }
