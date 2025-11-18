e58ffaaf07285d792bb71085f249cd79f60e1681
MattMark <matthew99729@gmail.com>
2025-11-18 10:56:16 -0800

    Fixed errors

--- FileHandler.c
+++ FileHandler.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <FileHandler.h>
-#include <Gradebook.h>
+#include <stdlib.h>
+#include "FileHandler.h"
+#include "Gradebook.h"
 
 void save(){
-	Student[] stuArray = getGradebook();					// Get students from gradebook
+	Student* stuArray = getGradebook();					// Get students from gradebook
 	int size = getSize();									// Get size of gradebook
-	Student *buffer = malloc(sizeOf(Student) * size); 		// Only allocate needed space
+	Student *buffer = malloc(sizeof(Student) * size); 		// Only allocate needed space
 
 	for (int i = 0; i < size; i++){							// For each element...
 		buffer[i] = stuArray[i];							// Copy data into buffer
@@ -13,8 +14,8 @@ void save(){
 	}
 
 	FILE *gradebookFile = fopen("Gradebook.save","w");		// Create/open gradebook file
-	fwrite(&size, sizeOf(int), 1, gradebookFile);			// Write size to file
-	fwrite(buffer, sizeOf(Student), size, gradebookFile);	// Write students to file
+	fwrite(&size, sizeof(int), 1, gradebookFile);			// Write size to file
+	fwrite(buffer, sizeof(Student), size, gradebookFile);	// Write students to file
 
 	fclose(gradebookFile);
 
@@ -26,11 +27,11 @@ void load(){
 		return;
 
 	int size;
-	Student[size] stuArray;
-	fread(&size, sizeOf(int), 1, gradebookFile);			// Get size from gradebookFile
-	fread(&stuArray, sizeOf(Student), size, gradebookFile);
+	fread(&size, sizeof(int), 1, gradebookFile);			// Get size from gradebookFile
+	Student stuArray[size];
+	fread(&stuArray, sizeof(Student), size, gradebookFile);
 
-	setGradebook(stuArray);
+	setGradebook(size, stuArray);
 
 	fclose(gradebookFile);
 
