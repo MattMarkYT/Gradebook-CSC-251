e5e8ba9fe956c582523b3fbbf822ffc34c4661a4
kmanalang2 <kmanalang2@toromail.csudh.edu>
2025-12-01 15:32:20 -0800

    Update Stats.h

--- Stats.h
+++ Stats.h
@@ -2,6 +2,7 @@
 #define STATS_H
 
 // 1. display the statistics of the class all at once (Reason: avoid unnecessary function calls)
-void ComputeStats(); // must pass struct Student, so that we can pass it to the functions inside this one
+void computeStats(); // must pass struct Student, so that we can pass it to the functions inside this one
 		     // in this case, we are passing by value. This is a no arg because we do not want dependency on inputting the struct array.
+
 #endif
